bd055cd67e5b26a7c5086f1eb7fa0a69e04ff872
Gokhan Korkmaz <mgokhankorkmaz@outlook.com>
2023-01-28 14:13:47 +0300

    entities created

---
+++ src/main/java/com/gokhankorkmaz/loanapplicationsystem/entities/Credit.java
@@ -0,0 +1,28 @@
+package com.gokhankorkmaz.loanapplicationsystem.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "credits")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Credit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "state")
+    private boolean state;
+    @Column(name = "amount")
+    private double amount;
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+}
---
+++ src/main/java/com/gokhankorkmaz/loanapplicationsystem/entities/Customer.java
@@ -0,0 +1,40 @@
+package com.gokhankorkmaz.loanapplicationsystem.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "customers")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private  int id;
+    @Column(name = "identity_number")
+    private String identityNumber;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    @Column(name = "montly_income")
+    private double monthlyIncome;
+    @Column(name = "birthdate")
+    private Date birthdate;
+    @Column(name = "assurance")
+    private double assurance;
+    @OneToMany(mappedBy = "customer")
+    private List<Credit> credits;
+
+}
